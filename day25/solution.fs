d4c3e79c1b7526b4631b6e9e1dee1b2653fe0262
Daniel Agg <daniel.agg@outlook.com>
2023-11-30 14:47:16 +0100

    init

---
+++ day25/solution.fs
@@ -0,0 +1,12 @@
+namespace Day25
+
+open System.IO
+
+
+module Part1 =
+    let run () =
+        1
+
+module Part2 =
+    let run () =
+        1
---
+++ day30/solution.fs
@@ -0,0 +1,12 @@
+namespace Day30
+
+open System.IO
+
+
+module Part1 =
+    let run () =
+        1
+
+module Part2 =
+    let run () =
+        1
