3e6d953ba9892a4b4b4146e15a79c7a53eb69fed
Daniel Agg <daniel.agg@outlook.com>
2023-12-01 18:14:14 +0100

    day1, part1

--- day1/solution.fs
+++ day1/solution.fs
@@ -1,11 +1,22 @@
 namespace Day01
 
 open System.IO
+open System
 
 
 module Part1 =
+    let extractValues (rawInput: string) =
+        let firstDigit = rawInput |> Seq.find Char.IsDigit
+        let lastDigit = rawInput |> Seq.findBack Char.IsDigit
+        let combinedString = string firstDigit + string lastDigit
+
+        int combinedString
+
     let run () =
-        1
+        let inputFilePath = Path.Combine(__SOURCE_DIRECTORY__, "input.txt")
+        let rawInput = File.ReadAllLines(inputFilePath)
+
+        Array.map extractValues rawInput |> Array.sum
 
 module Part2 =
     let run () =
